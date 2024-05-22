9dae5885b3a68f69fd596f814dcb78e3e747d619
MoFaraz <mopharaz@gmail.com>
2024-05-22 12:01:02 +0330

    add users controller and service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { CreateUserDto } from './dto/createUser.dto';
+import { User } from '@prisma/client';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Get()
+    @ApiOperation({ summary: 'Get All Users'})
+    @ApiResponse({ status:200, description: 'Success'/*, type: User */})
+    async findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    @ApiOperation({ summary: 'Get a user by ID' })
+    @ApiResponse({ status: 200, description: 'Success'/*, type: User */})
+    async findOne(@Param('id') id: string): Promise<User> {
+      return this.usersService.findById(id);
+    }
+
+    @Post()
+    @ApiOperation({ summary: 'Create a new user' })
+    @ApiResponse({ status: 201, description: 'Created'/*, type: User */})
+    async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+      return this.usersService.createUser(createUserDto);
+    }
+
+    @Put(':id')
+    @ApiOperation({ summary: 'Update a user by ID' })
+    @ApiResponse({ status: 200, description: 'Updated'/*, type: User */})
+    async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+        return this.usersService.updateUser(id, updateUserDto);
+    }
+
+    @Delete(':id')
+    @ApiOperation({ summary: 'Delete a user by ID' })
+    @ApiResponse({ status: 200, description: 'Deleted'/*, type: User */})
+    async delete(@Param('id') id: string): Promise<User> {
+        return this.usersService.deleteUser(id);
+    }
+
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prismaService: PrismaService){}
+
+    async createUser (createUserDto: CreateUserDto): Promise<User> {
+        const {username ,email ,password ,role} = createUserDto;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        return this.prismaService.user.create({
+            data: {
+              username,
+              email,
+              password: hashedPassword,
+              role,
+            },
+          });
+    }
+
+  async updateUser(id: string ,updateUser: UpdateUserDto): Promise<User> {
+    const {username, email, password, role} = updateUser;
+    const hashedPassword = (password) ? await bcrypt.hash(password, 10) : undefined;
+    return this.prismaService.user.update({
+      where: { id },
+      data: {
+        email,
+        password: hashedPassword,
+        username,
+        role,
+      }
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  async findById(id: string): Promise<User|undefined> {
+    return this.prismaService.user.findUnique({ where: { id }});
+  }
+
+  async findByUsername(username: string): Promise<User|undefined> {
+    return this.prismaService.user.findUnique({ where: { username }});
+  }
+
+  async deleteUser(id: string): Promise<User> {
+    return this.prismaService.user.delete({ where: { id }});
+  }
+}
