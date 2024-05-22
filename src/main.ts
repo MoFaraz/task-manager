308b9bf7a0832279922b00a91af4432735e38365
MoFaraz <mopharaz@gmail.com>
2024-05-22 12:29:27 +0330

    add swagger config to the main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { setupSwagger } from './swagger.config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(this.app);
   await app.listen(3000);
 }
 bootstrap();
